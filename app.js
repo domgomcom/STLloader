eecf1ee319a30080924b57de02ee0d079f301b67
domgomcom <domgom.com@gmail.com>
2022-10-13 01:49:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import * as THREE from "https://cdn.skypack.dev/three@0.129.0";
 const scene = new THREE.scene();
 //console.log(scene);
 const camera = new THREE.PerspectiveCamera(35, window.innerWidth/window.innerHeight,0.1,1000);
-const light = new THREE.DirectionalLight({color:0xffffff, 0.5});
+const light = new THREE.DirectionalLight(0xffffff, 0.5);
 const render = new THREE.WebGLRenderer();
 
 const box = new THREE.BoxGeometry(1,1,1);
