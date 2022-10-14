ac9b290993beea93b348655f086d5227bb4fea60
domgomcom <domgom.com@gmail.com>
2022-10-14 04:36:10 +0200

    1

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function animate(){
 }
 
 let loader = new STLLoader();
-loader.load('./3dmodels/Baby_Yoda_v2.2.stl', (model)=>{
+loader.load('./3dmodels/Cersei.stl', (model)=>{
     object = new THREE.Mesh(
         model,
         new THREE.MeshLambertMaterial({color: 0x00ff00})
